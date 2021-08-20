f348a966326d895eb50fa449f431961b14a21ece
Oktaviardi <oktaviardi2510@yahoo.co.id>
2021-08-20 13:21:58 +0700

    renamed

--- server.js
+++ server.js
@@ -28,7 +28,7 @@ mongoose.connect(dbConfig.url, {
 
 // define a simple route
 app.get('/', (req, res) => {
-    res.json({"message": "Welcome to EasyNotes application. Take notes quickly. Organize and keep track of all your notes."});
+    res.json({"message": "Welcome to expreess_with_mongodb application. Take notes quickly. Organize and keep track of all your notes."});
 });
 
 // Require Notes routes
